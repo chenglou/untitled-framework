c9501e30fd3e5091c0c8419acc62c705e1e88df7
Cheng Lou <chenglou92@gmail.com>
2024-11-11 05:15:17 -0800

    Fix formatting sigh

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,4 +3,5 @@ export default {
   printWidth: 120,
   semi: false,
   singleQuote: true,
+  experimentalTernaries: true, // prettier v3 broke multi-ternaries. Enable this to fix it: https://prettier.io/blog/2023/11/13/curious-ternaries
 }
