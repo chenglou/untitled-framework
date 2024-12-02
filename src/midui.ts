6936afff36ea72de3ff0e5236af6af6496abf818
Cheng Lou <chenglou92@gmail.com>
2024-12-02 11:41:05 -0500

    Slight cleanup of midui

--- src/midui.ts
+++ src/midui.ts
@@ -1,14 +1,21 @@
 // === generic scheduler & its debugger
 export function makeScheduler<K extends keyof WindowEventMap>(
   events: Array<K>,
-  render: ((now: number, events: { [K in keyof WindowEventMap]: WindowEventMap[K] | null }, animationSteps: number) => boolean),
+  render: (
+    now: number,
+    events: { [K in keyof WindowEventMap]: WindowEventMap[K] | null },
+    animationSteps: number,
+  ) => boolean,
 ) {
   let scheduledRender = false
 
   let initEvents = {} as { [K in keyof WindowEventMap]: WindowEventMap[K] | null }
   for (const key of events) {
     initEvents[key] = null
-    window.addEventListener(key, (e) => { initEvents[key] = e; scheduleRender() })
+    window.addEventListener(key, (e) => {
+      initEvents[key] = e
+      scheduleRender()
+    })
   }
 
   let animatedUntilTime: number | null = null
@@ -17,7 +24,8 @@ export function makeScheduler<K extends keyof WindowEventMap>(
     if (scheduledRender) return
     scheduledRender = true
 
-    requestAnimationFrame(function renderAndMaybeScheduleAnotherRender(now) { // eye-grabbing name. No "(anonymous)" function in the debugger & profiler
+    requestAnimationFrame(function renderAndMaybeScheduleAnotherRender(now) {
+      // eye-grabbing name. No "(anonymous)" function in the debugger & profiler
       scheduledRender = false
 
       let newAnimatedUntilTime = animatedUntilTime ?? now
@@ -38,10 +46,10 @@ export function makeScheduler<K extends keyof WindowEventMap>(
 // 4ms/step for the spring animation's step. Typically 4 steps for 60fps (16.6ms/frame) and 2 for 120fps (8.3ms/frame). Frame time delta varies, so not always true
 // could use 8ms instead, but 120fps' 8.3ms/frame means the computation might not fit in the remaining 0.3ms, which means sometime the simulation step wouldn't even run once, giving the illusion of jank
 export const msPerAnimationStep = 6
-export type Spring = { pos: number, dest: number, v: number, k: number, b: number }
+export type Spring = { pos: number; dest: number; v: number; k: number; b: number }
 // function spring(pos: number, v = 0, k = 290, b = 30): SpringConfig {
-export function spring(pos: number, v = 0, k = 333, b = 33): Spring {
-  return { pos, dest: pos, v, k, b } // k = stiffness, b = damping. Try https://chenglou.me/react-motion/demos/demo5-spring-parameters-chooser/
+export function spring(pos: number, dest = pos, v = 0, k = 333, b = 33): Spring {
+  return { pos, dest, v, k, b } // k = stiffness, b = damping. Try https://chenglou.me/react-motion/demos/demo5-spring-parameters-chooser/
 }
 export function springStep(config: Spring) {
   // https://blog.maximeheckel.com/posts/the-physics-behind-spring-animations/
@@ -55,7 +63,8 @@ export function springStep(config: Spring) {
   const newV = v + a * t
   const newPos = pos + newV * t
 
-  config.pos = newPos; config.v = newV
+  config.pos = newPos
+  config.v = newV
 }
 export function springGoToEnd(config: Spring) {
   config.pos = config.dest
@@ -73,7 +82,7 @@ export function center(containee: number, container: number, containerInsetStart
 
 export function remap(value: number, oldMin: number, oldMax: number, newMin: number, newMax: number): number {
   if (oldMin === oldMax) return (newMin + newMax) / 2 // avoid divide by 0
-  return (value - oldMin) / (oldMax - oldMin) * (newMax - newMin) + newMin
+  return ((value - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin
 }
 
 export function length(x: number, y: number) {
@@ -83,16 +92,19 @@ export function lessEqual(a: number, b: number, c: number) {
   return a <= b && b <= c
 }
 
-export function insideInclusive(
-  x1: number, y1: number,
-  x2: number, y2: number, sizeX: number, sizeY: number,
-) {
+export function insideInclusive(x1: number, y1: number, x2: number, y2: number, sizeX: number, sizeY: number) {
   return x2 <= x1 && x1 <= x2 + sizeX && y2 <= y1 && y1 <= y2 + sizeY
 }
 
 export function overlapInclusive(
-  x1: number, y1: number, sizeX1: number, sizeY1: number,
-  x2: number, y2: number, sizeX2: number, sizeY2: number,
+  x1: number,
+  y1: number,
+  sizeX1: number,
+  sizeY1: number,
+  x2: number,
+  y2: number,
+  sizeX2: number,
+  sizeY2: number,
 ) {
   return x1 <= x2 + sizeX2 && x2 <= x1 + sizeX1 && y1 <= y2 + sizeY2 && y2 <= y1 + sizeY1
 }
@@ -103,7 +115,11 @@ export function fit(ar: number, containerSizeX: number, containerSizeY: number) 
 }
 
 export function clamp(min: number, v: number, max: number) {
-  return v > max ? max : v < min ? min : v
+  return (
+    v > max ? max
+    : v < min ? min
+    : v
+  )
 }
 
 export function easeOutQuad(x: number): number {
@@ -134,6 +150,13 @@ export function fract(x: number) {
   return x - Math.floor(x)
 }
 
+// https://www.shadertoy.com/view/4djSRW
+export function hash11(p: number) {
+  p = fract(p * 0.1031)
+  p *= p + 33.33
+  p *= p + p
+  return fract(p)
+}
 // export function hash12(a: number, b: number) {
 //   let aa = fract(a * .1031)
 //   let bb = fract(b * .1031)
@@ -145,4 +168,4 @@ export function fract(x: number) {
 // }
 
 // convenient types
-export type XY = { x: number, y: number }
+export type XY = { x: number; y: number }
