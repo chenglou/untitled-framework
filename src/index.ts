3f0263b670a2a78c0c634c7ff2e26717c5603702
Cheng Lou <chenglou92@gmail.com>
2025-02-05 01:11:39 -0800

    Undo the scheduler abstraction. I don't like it

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { center, hash11, makeScheduler } from './martian'
-import { Spring, spring, springGoToEnd, springStep } from './martian/spring'
+import { center, hash11 } from './martian'
+import { msPerAnimationStep, Spring, spring, springGoToEnd, springStep } from './martian/spring'
 
 // === constant layout metrics. The rest is dynamic
 const rowSizeX = 320
@@ -32,8 +32,6 @@ const state: State = {
   pointer: [{ x: 0, y: 0, time: 0 }], // circular buffer. On page load, there's no way to render a first cursor state =(
   data: [],
 }
-
-// === initialize data
 {
   const windowSizeX = document.documentElement.clientWidth // excludes scroll bar & invariant under safari pinch zoom
   for (let i = 0; i < 5; i++) {
@@ -56,6 +54,65 @@ const state: State = {
     document.body.appendChild(node)
   }
 }
+
+// Global events object to capture incoming events. These are reset at the end of every frame
+const events: {
+  mouseup: MouseEvent | null
+  touchend: TouchEvent | null
+  mousemove: MouseEvent | null
+  touchmove: TouchEvent | null
+  pointerdown: PointerEvent | null
+} = {
+  mouseup: null,
+  touchend: null,
+  mousemove: null,
+  touchmove: null,
+  pointerdown: null,
+}
+
+// Add event listeners to update the events object and trigger rendering
+window.addEventListener('mouseup', (e) => {
+  events.mouseup = e
+  scheduleRender()
+})
+window.addEventListener('touchend', (e) => {
+  events.touchend = e
+  scheduleRender()
+})
+window.addEventListener('mousemove', (e) => {
+  events.mousemove = e
+  scheduleRender()
+})
+window.addEventListener('touchmove', (e) => {
+  events.touchmove = e
+  scheduleRender()
+})
+window.addEventListener('pointerdown', (e) => {
+  events.pointerdown = e
+  scheduleRender()
+})
+
+let scheduledRender = false
+let animatedUntilTime: number | null = null
+function scheduleRender() {
+  if (scheduledRender) return
+  scheduledRender = true
+  requestAnimationFrame(function renderAndMaybeScheduleAnotherRender(now) {
+    // eye-grabbing name. No "(anonymous)" function in the debugger & profiler
+    scheduledRender = false
+
+    let newAnimatedUntilTime = animatedUntilTime ?? now
+    const animationSteps = Math.floor((now - newAnimatedUntilTime) / msPerAnimationStep)
+    newAnimatedUntilTime += animationSteps * msPerAnimationStep
+    const stillAnimating = render(now, animationSteps)
+    animatedUntilTime = stillAnimating ? newAnimatedUntilTime : null
+
+    // Reset events after they've been processed
+    events.mouseup = events.touchend = events.mousemove = events.touchmove = events.pointerdown = null
+    if (stillAnimating) scheduleRender()
+  })
+}
+
 function springForEach(f: (s: Spring) => void) {
   for (const d of state.data) {
     f(d.x)
@@ -72,134 +129,132 @@ function hitTest(data: DataItem[], pointer: PointerPoint): DataItem | void {
   }
 }
 
-// pointermove doesn't work on android, pointerdown isn't fired on Safari on the first left click after dismissing context menus, mousedown doesn't trigger properly on mobile, pointerup isn't triggered when pointer panned (at least on iOS), don't forget contextmenu event. Tldr there's no pointer event that works cross-browser that can replace mouse & touch events.
-const scheduleRender = makeScheduler(
-  ['mouseup', 'touchend', 'mousemove', 'touchmove', 'pointerdown'],
-  (now, events, animationSteps) => {
-    // === step 0: process events
-    // mouseup/touchend
-    // move
-    // when scrolling (which might schedule a render), a container's pointermove doesn't trigger, so the pointer's local coordinates are stale
-    // this means we should only use pointer's global coordinates, which is always right
-    if (events.mouseup || events.touchend) state.pointerState = 'up'
-    if (events.mousemove)
-      state.pointer.push({ x: events.mousemove.pageX, y: events.mousemove.pageY, time: performance.now() })
-    if (events.touchmove)
-      state.pointer.push({
-        x: events.touchmove.touches[0]!.pageX,
-        y: events.touchmove.touches[0]!.pageY,
-        time: performance.now(),
-      })
-    if (events.pointerdown) {
-      state.pointerState = 'firstDown'
-      state.pointer.push({ x: events.pointerdown.pageX, y: events.pointerdown.pageY, time: performance.now() })
-    }
-
-    // === step 1: batched DOM reads (to avoid accidental DOM read & write interleaving)
-    const windowSizeX = document.documentElement.clientWidth // excludes scroll bar & invariant under safari pinch zoom
-    const pointerLast = state.pointer.at(-1)! // guaranteed non-null since pointer.length >= 1
-
-    // === step 2: handle inputs-related state change
-    let newDragged: DraggedInfo | null = null
-    if (state.pointerState === 'down') newDragged = state.dragged
-    else if (state.pointerState === 'up') {
-      if (state.dragged != null) {
-        // if we just dragged & released an item, give it a bit of flick velocity based on how fast we swiped it away
-        const dragIdx = state.data.findIndex((d) => d.id === state.dragged!.id)
-        let i = state.pointer.length - 1
-        while (i >= 0 && now - state.pointer[i]!.time <= 100) i-- // only consider last ~100ms of movements
-        const pointer = state.pointer[i]!
-        const deltaTime = now - pointer.time
-        const vx = ((pointerLast.x - pointer.x) / deltaTime) * 1000 // speed over ~1s
-        const vy = ((pointerLast.y - pointer.y) / deltaTime) * 1000
-        state.data[dragIdx]!.x.v += vx
-        state.data[dragIdx]!.y.v += vy
-      }
-      newDragged = null
-    } else {
-      const hit = hitTest(state.data, pointerLast)
-      if (hit) newDragged = { id: hit.id, deltaX: pointerLast.x - hit.x.pos, deltaY: pointerLast.y - hit.y.pos }
-    }
-
-    // === step 3: calculate new layout & cursor
-    if (newDragged) {
-      // first, swap row based on cursor position if needed
-      let dragIdx = state.data.findIndex((d) => d.id === newDragged.id) // guaranteed non-null
-      const d = state.data[dragIdx]!
-      const x = pointerLast.x - newDragged.deltaX
-      const y = pointerLast.y - newDragged.deltaY
-      d.x.pos = d.x.dest = x + (center(rowSizeX, windowSizeX) - x) / 1.5 // restrict horizontal drag a bit
-      d.y.pos = d.y.dest = y
-      d.scale.dest = 1.1
-      // dragging row upward? Swap it with previous row if cursor is above midpoint of previous row
-      while (dragIdx > 0 && pointerLast.y < state.data[dragIdx - 1]!.y.dest + state.data[dragIdx - 1]!.sizeY / 2) {
-        ;[state.data[dragIdx], state.data[dragIdx - 1]] = [state.data[dragIdx - 1]!, state.data[dragIdx]!] // swap
-        dragIdx--
-      }
-      // dragging row downward? Swap it with next row if cursor is below midpoint of next row
-      while (
-        dragIdx < state.data.length - 1 &&
-        pointerLast.y > state.data[dragIdx + 1]!.y.dest + state.data[dragIdx + 1]!.sizeY / 2
-      ) {
-        ;[state.data[dragIdx], state.data[dragIdx + 1]] = [state.data[dragIdx + 1]!, state.data[dragIdx]!] // swap
-        dragIdx++
-      }
-    }
-    let top = windowPaddingTop
-    for (const d of state.data) {
-      if (newDragged && d.id === newDragged.id) {
-        // already modified above for the dragged element
-      } else {
-        d.x.dest = center(rowSizeX, windowSizeX)
-        d.y.dest = top
-        d.scale.dest = 1
-      }
-      top += d.sizeY
-    }
-    const cursor =
-      newDragged ?
-        'grabbing' // will be "grabbing" even if pointer leaves the card
-      : hitTest(state.data, pointerLast) ? 'grab'
-      : 'auto'
-
-    // === step 4: run animation
-    let stillAnimating = false
-    springForEach((s) => {
-      for (let i = 0; i < animationSteps; i++) springStep(s)
-      if (Math.abs(s.v) < 0.01 && Math.abs(s.dest - s.pos) < 0.01)
-        springGoToEnd(s) // close enough, we're done
-      else stillAnimating = true
+// this means we should only use pointer's global coordinates, which is always right
+function render(now: number, animationSteps: number): boolean {
+  // === step 0: process events
+  // mouseup/touchend
+  // move
+  // when scrolling (which might schedule a render), a container's pointermove doesn't trigger, so the pointer's local coordinates are stale
+  // this means we should only use pointer's global coordinates, which is always right
+  if (events.mouseup || events.touchend) state.pointerState = 'up'
+  if (events.mousemove)
+    state.pointer.push({ x: events.mousemove.pageX, y: events.mousemove.pageY, time: performance.now() })
+  if (events.touchmove)
+    state.pointer.push({
+      x: events.touchmove.touches[0]!.pageX,
+      y: events.touchmove.touches[0]!.pageY,
+      time: performance.now(),
     })
+  if (events.pointerdown) {
+    state.pointerState = 'firstDown'
+    state.pointer.push({ x: events.pointerdown.pageX, y: events.pointerdown.pageY, time: performance.now() })
+  }
 
-    // === step 5: render. Batch DOM writes
-    for (let i = 0; i < state.data.length; i++) {
-      const d = state.data[i]!
-      const style = d.node.style
+  // === step 1: batched DOM reads (to avoid accidental DOM read & write interleaving)
+  const windowSizeX = document.documentElement.clientWidth // excludes scroll bar & invariant under safari pinch zoom
+  const pointerLast = state.pointer.at(-1)! // guaranteed non-null since pointer.length >= 1
 
-      // the visual scaling up is to simulate that the item is getting closer to us. They should therefore have a higher z-index
-      const zIndex = Math.floor(d.scale.pos * 100) // [1, 1.1] -> [100, 110]
-      style.zIndex = `${zIndex}`
-
-      style.transform = `translate3d(${d.x.pos}px,${d.y.pos}px,0) scale(${d.scale.pos})`
-      if (newDragged && d.id === newDragged.id) {
-        style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 16px 32px 0px'
-        style.opacity = '0.7'
-      } else {
-        style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px'
-        style.opacity = '1.0'
-      }
+  // === step 2: handle inputs-related state change
+  let newDragged: DraggedInfo | null = null
+  if (state.pointerState === 'down') newDragged = state.dragged
+  else if (state.pointerState === 'up') {
+    if (state.dragged != null) {
+      // if we just dragged & released an item, give it a bit of flick velocity based on how fast we swiped it away
+      const dragIdx = state.data.findIndex((d) => d.id === state.dragged!.id)
+      let i = state.pointer.length - 1
+      while (i >= 0 && now - state.pointer[i]!.time <= 100) i-- // only consider last ~100ms of movements
+      const pointer = state.pointer[i]!
+      const deltaTime = now - pointer.time
+      const vx = ((pointerLast.x - pointer.x) / deltaTime) * 1000 // speed over ~1s
+      const vy = ((pointerLast.y - pointer.y) / deltaTime) * 1000
+      state.data[dragIdx]!.x.v += vx
+      state.data[dragIdx]!.y.v += vy
     }
-    document.body.style.cursor = cursor
+    newDragged = null
+  } else {
+    const hit = hitTest(state.data, pointerLast)
+    if (hit) newDragged = { id: hit.id, deltaX: pointerLast.x - hit.x.pos, deltaY: pointerLast.y - hit.y.pos }
+  }
 
-    // === step 6: update state & prepare for next frame
-    if (state.pointerState === 'firstDown') state.pointerState = 'down'
-    if (state.dragged && newDragged == null) state.lastDragged = state.dragged
-    state.dragged = newDragged
-    if (state.pointerState === 'up') state.pointer = [{ x: 0, y: 0, time: 0 }]
-    if (state.pointer.length > 20) state.pointer.shift() // keep only the last ~20 pointer events
+  // === step 3: calculate new layout & cursor
+  if (newDragged) {
+    // first, swap row based on cursor position if needed
+    let dragIdx = state.data.findIndex((d) => d.id === newDragged.id) // guaranteed non-null
+    const d = state.data[dragIdx]!
+    const x = pointerLast.x - newDragged.deltaX
+    const y = pointerLast.y - newDragged.deltaY
+    d.x.pos = d.x.dest = x + (center(rowSizeX, windowSizeX) - x) / 1.5 // restrict horizontal drag a bit
+    d.y.pos = d.y.dest = y
+    d.scale.dest = 1.1
+    // dragging row upward? Swap it with previous row if cursor is above midpoint of previous row
+    while (dragIdx > 0 && pointerLast.y < state.data[dragIdx - 1]!.y.dest + state.data[dragIdx - 1]!.sizeY / 2) {
+      ;[state.data[dragIdx], state.data[dragIdx - 1]] = [state.data[dragIdx - 1]!, state.data[dragIdx]!] // swap
+      dragIdx--
+    }
+    // dragging row downward? Swap it with next row if cursor is below midpoint of next row
+    while (
+      dragIdx < state.data.length - 1 &&
+      pointerLast.y > state.data[dragIdx + 1]!.y.dest + state.data[dragIdx + 1]!.sizeY / 2
+    ) {
+      ;[state.data[dragIdx], state.data[dragIdx + 1]] = [state.data[dragIdx + 1]!, state.data[dragIdx]!] // swap
+      dragIdx++
+    }
+  }
+  let top = windowPaddingTop
+  for (const d of state.data) {
+    if (newDragged && d.id === newDragged.id) {
+      // already modified above for the dragged element
+    } else {
+      d.x.dest = center(rowSizeX, windowSizeX)
+      d.y.dest = top
+      d.scale.dest = 1
+    }
+    top += d.sizeY
+  }
+  const cursor =
+    newDragged ?
+      'grabbing' // will be "grabbing" even if pointer leaves the card
+    : hitTest(state.data, pointerLast) ? 'grab'
+    : 'auto'
 
-    return stillAnimating
-  },
-)
+  // === step 4: run animation
+  let stillAnimating = false
+  springForEach((s) => {
+    for (let i = 0; i < animationSteps; i++) springStep(s)
+    if (Math.abs(s.v) < 0.01 && Math.abs(s.dest - s.pos) < 0.01)
+      springGoToEnd(s) // close enough, we're done
+    else stillAnimating = true
+  })
 
+  // === step 5: render. Batch DOM writes
+  for (let i = 0; i < state.data.length; i++) {
+    const d = state.data[i]!
+    const style = d.node.style
+
+    // the visual scaling up is to simulate that the item is getting closer to us. They should therefore have a higher z-index
+    const zIndex = Math.floor(d.scale.pos * 100) // [1, 1.1] -> [100, 110]
+    style.zIndex = `${zIndex}`
+
+    style.transform = `translate3d(${d.x.pos}px,${d.y.pos}px,0) scale(${d.scale.pos})`
+    if (newDragged && d.id === newDragged.id) {
+      style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 16px 32px 0px'
+      style.opacity = '0.7'
+    } else {
+      style.boxShadow = 'rgba(0, 0, 0, 0.2) 0px 1px 2px 0px'
+      style.opacity = '1.0'
+    }
+  }
+  document.body.style.cursor = cursor
+
+  // === step 6: update state & prepare for next frame
+  if (state.pointerState === 'firstDown') state.pointerState = 'down'
+  if (state.dragged && newDragged == null) state.lastDragged = state.dragged
+  state.dragged = newDragged
+  if (state.pointerState === 'up') state.pointer = [{ x: 0, y: 0, time: 0 }]
+  if (state.pointer.length > 20) state.pointer.shift() // keep only the last ~20 pointer events
+
+  return stillAnimating
+}
+
+// Start the render loop
 scheduleRender()
